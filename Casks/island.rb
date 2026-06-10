f9f30de7e23894d8de2dfcad784c5bc0521e868c
ship-release <noreply@ship>
2026-06-10 22:01:13 +0900

    island 0.2.0

--- Casks/island.rb
+++ Casks/island.rb
@@ -1,6 +1,6 @@
 cask "island" do
-  version "0.1.6"
-  sha256 "faa3ae81d3909c50c191cfaf417fb4e75f2c137931f79b6ac616a625c714ea61"
+  version "0.2.0"
+  sha256 "2daf650556bc60543aaf4fd48479a9ddbcf477c70d8b5f833b5f7568d57ce34f"
 
   url "https://github.com/hoemoon/homebrew-island/releases/download/v#{version}/island.zip"
   name "island"
